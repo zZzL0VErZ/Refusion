367942897de7a989f48a9255038d0e6cd01dab69
xvSync <71209712+xvSync@users.noreply.github.com>
2021-04-07 14:06:30 +0100

    Create ubuntusetup.sh

---
+++ ubuntusetup.sh
@@ -0,0 +1,6 @@
+echo xdddddddddddddd
+echo for centos server pls do 'sh centossetup.sh
+sudo apt-get install iptables
+sudo service iptables start
+sudo service firewalld start
+echo done qwe qwe qwe qw now paste the proiptables text into your console
